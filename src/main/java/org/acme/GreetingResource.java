2a8001ff5e4323c721cf6e5a15b755f7413461bb
경민 <kyungmin1113@gmail.com>
2026-04-29 11:29:36 +0900

    9주차 (MySQL 연결해야함)

--- src/main/java/org/acme/GreetingResource.java
+++ src/main/java/org/acme/GreetingResource.java
@@ -5,10 +5,10 @@ import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 
-@Path("/hello")
+@Path("/hello") //어노테이션
 public class GreetingResource {
 
-    @GET
+    @GET //어노테이션
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
         return "Hello from Quarkus REST";
